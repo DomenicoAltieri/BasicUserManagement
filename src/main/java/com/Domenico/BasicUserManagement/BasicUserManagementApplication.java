2d823a629cc3ed92bbd82e1056c94b5a8379dcd1
Domenico Altieri <domenico.altieri.mail@gmail.com>
2021-11-12 18:54:49 +0100

    Added Spring Boot Files

---
+++ src/main/java/com/Domenico/BasicUserManagement/BasicUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Domenico.BasicUserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Domenico/BasicUserManagement/BasicUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Domenico.BasicUserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

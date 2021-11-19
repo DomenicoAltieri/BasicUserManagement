3fa8c6499b707540532899dff461694b43bc7f22
Domenico Altieri <domenico.altieri.mail@gmail.com>
2021-11-19 15:39:40 +0100

    Added a method for creating a User entry

--- src/main/java/com/Domenico/BasicUserManagement/User/UserController.java
+++ src/main/java/com/Domenico/BasicUserManagement/User/UserController.java
@@ -3,9 +3,7 @@ package com.Domenico.BasicUserManagement.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -14,14 +12,14 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping ("/users")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping ("/dbinit")
-    void databaseInitialization ()	{
+    @GetMapping("/dbinit")
+    void databaseInitialization() {
         User Marco = new User("Marco", "", "Toscano", "marco.toscano@outlook.com",
                 LocalDate.of(2010, Month.APRIL, 4));
         User Donato = new User("Donato", "", "Rossi", "donato.rossi@gmail.com",
@@ -48,6 +46,15 @@ public class UserController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
 
+    @PostMapping
+    ResponseEntity<User> createUserEntry(@RequestBody User newUser) {
+        try {
+            userRepository.save(newUser);
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
